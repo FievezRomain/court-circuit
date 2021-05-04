eb3eaa62817b5c41bb344f755d7d00703a1c7cd6
InesAhdjoudj <57198921+InesAhdjoudj@users.noreply.github.com>
2021-05-04 20:02:56 +0200

    Ajout de la page catalogue

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -8,9 +8,19 @@ import { ProductService } from '../services/product.service';
 })
 export class CatalogComponent implements OnInit {
 
+  
+
+  product1: any = {idArticle : 0, titre : "Pomme", categorie : "Fruit", description : "Mon premier produit", provenance : "France", prix : 10, image : "assets/img/product1.jpg"}
+  product2: any = {idArticle : 1, titre: "Pomme2", categorie : "Fruit", description : "Mon second produit", provenance : "Italie", prix : 15, image : "assets/img/product1.jpg"}
+  products : Array<any> = new Array <any>();
+
+
   constructor(public productService : ProductService) { }
 
   ngOnInit(): void {
+    this.products.push(this.product1, this.product2);
   }
-
+  addToCard(idArticle: number):void{
+    //Add product to user concerned
+  }
 }
